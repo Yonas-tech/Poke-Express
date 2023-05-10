15dc077387f9595e385e8fd53d32b6220b9bf0aa
Yonas-coder <52844663+Yonas-coder@users.noreply.github.com>
2023-05-10 17:15:23 -0400

    New Pokemon Page created, shows up at /pokemon/new

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ const port = 3000;
 // Data:
 const pokemon = require("./models/pokemon")
 
+// Middleware:
+app.use((req,res,next)=>{
+    next();
+})
+app.use(express.urlencoded({extended:false}));
+
+
+//JSX view engine:
 app.set("view engine", "jsx")
 app.engine("jsx",require('jsx-view-engine').createEngine() ) 
 
+
+// Routes:
 
 // include a get route / that will res.send('Welcome to the Pokemon App!'); so that when you got to localhost:3000, you will see Welcome to the Pokemon App!
 app.get('/',(req,res)=>{
@@ -22,10 +32,23 @@ app.get('/pokemon', (req,res)=>{
     res.render('Index',{pokemon: pokemon})
 })
 
+app.get('/pokemon/new',(req,res)=>{
+    res.render('New');
+})
+
+
 app.get('/pokemon/:id', (req,res)=>{
     // res.send(req.params.id)
     res.render('Show',{pokemon: pokemon[req.params.id]})
 })
+
+
+// POST:
+app.post('/pokemon', (req, res)=>{
+    pokemon.push(req.body);
+    console.log(pokemon)
+})
+
 
 // set your app to listen to the port and include a console.log(), so that you can tell when your server is running
 app.listen(port, ()=>{
---
+++ views/New.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class New extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add A New Pokemon To The Database</h1>
+        <form action="/pokemon" method='POST'>
+            Pokemon Name: <input type="text" name='name' id='name'/><br/>
+            Image URL: <input type="text" name="imgURL" id="imgURL" /><br/>
+            <input type="submit" value='Add Pokemon'/><br/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default New
