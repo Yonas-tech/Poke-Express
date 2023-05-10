b58e1dc45ce154a70edb95896610afd54d1bc4f3
Yonas-coder <52844663+Yonas-coder@users.noreply.github.com>
2023-05-10 16:22:49 -0400

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import pokemon from '../models/pokemon';
 
 export class Index extends Component {
   render() {
@@ -10,10 +11,26 @@ export class Index extends Component {
         padding: 0,
         margin: 0,
         };
+        
+// The name of each pokemon, as a list item, inside an unordered list
+// This list should be dynamically rendered by jsx based on your data from your 'database'
+// You'll notice the pokemon names aren't properly capitalized! Let's fix that! Manipulate the data programatically to capitalize the first letter of their names
+
 
     return (
       <div style={myStyle}>
         <h1>See All The Pokemon!</h1>
+        <ul>
+           {pokemon.map((poke)=>{
+            return(
+              <li>
+                {poke.name.charAt(0).toLocaleUpperCase() + poke.name.slice(1)}
+              </li>
+            )
+            })}
+        </ul>
+       
+          
       </div>
     )
   }
